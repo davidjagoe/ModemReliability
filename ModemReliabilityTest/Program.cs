845fbeebd57ad3d5b656bf18c09ba7146371a2ea
David Jagoe <davidjagoe@gmail.com>
2012-06-12 12:36:38 -0700

    More results

--- ModemReliabilityTest/Program.cs
+++ ModemReliabilityTest/Program.cs
@@ -26,7 +26,10 @@ namespace ModemReliabilityTest
                 log.Info("Running download");
                 try
                 {
-                    r = (HttpWebRequest)WebRequest.Create("http://drillersdashboard.com/images/productivity-circle.png");
+                    //string url = "http://ftp.gnu.org/gnu/emacs/windows/libxpm-3.5.8-w32-src.zip"; // 1.4 MB
+                    //string url = "http://drillersdashboard.com/images/productivity-circle.png"; // 125 kB
+                    string url = "http://ftp.gnu.org/gnu/emacs/windows/libxpm-3.5.8-w32-src.zip.sig"; // 96 bytes
+                    r = (HttpWebRequest)WebRequest.Create(url);
                     using (var resp = r.GetResponse())
                     using (var s = resp.GetResponseStream())
                     using (var sr = new StreamReader(s))
