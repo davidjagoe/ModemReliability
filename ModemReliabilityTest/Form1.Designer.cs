c3f6821d5cc5b096b6ac96e8689c0b00c8b6aed6
David Jagoe <davidjagoe@gmail.com>
2013-01-14 11:25:55 -0800

    Asyn read from httpwebresponse in small blocks

--- ModemReliabilityTest/Form1.Designer.cs
+++ ModemReliabilityTest/Form1.Designer.cs
@@ -29,14 +29,10 @@
         private void InitializeComponent()
         {
             this.dns_total = new System.Windows.Forms.Label();
-            this.nodns_total = new System.Windows.Forms.Label();
             this.dns_failures = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.dns_tot = new System.Windows.Forms.Label();
-            this.ip_tot = new System.Windows.Forms.Label();
-            this.dns_fail = new System.Windows.Forms.Label();
-            this.ip_fail = new System.Windows.Forms.Label();
+            this.n_requests = new System.Windows.Forms.Label();
+            this.n_failures = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dns_total
@@ -45,15 +41,8 @@
             this.dns_total.Location = new System.Drawing.Point(17, 13);
             this.dns_total.Name = "dns_total";
             this.dns_total.Size = new System.Drawing.Size(169, 20);
-            this.dns_total.Text = "Total Req - with DNS";
-            // 
-            // nodns_total
-            // 
-            this.nodns_total.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
-            this.nodns_total.Location = new System.Drawing.Point(17, 56);
-            this.nodns_total.Name = "nodns_total";
-            this.nodns_total.Size = new System.Drawing.Size(169, 20);
-            this.nodns_total.Text = "Total Req - no DNS";
+            this.dns_total.Text = "Total Req";
+            this.dns_total.ParentChanged += new System.EventHandler(this.dns_total_ParentChanged);
             // 
             // dns_failures
             // 
@@ -61,15 +50,7 @@
             this.dns_failures.Location = new System.Drawing.Point(17, 104);
             this.dns_failures.Name = "dns_failures";
             this.dns_failures.Size = new System.Drawing.Size(169, 20);
-            this.dns_failures.Text = "failures - with DNS";
-            // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(17, 148);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 20);
-            this.label1.Text = "failures - no DNS";
+            this.dns_failures.Text = "Failures";
             // 
             // button1
             // 
@@ -80,49 +61,31 @@
             this.button1.Text = "Exit";
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // dns_tot
+            // n_requests
             // 
-            this.dns_tot.Location = new System.Drawing.Point(207, 13);
-            this.dns_tot.Name = "dns_tot";
-            this.dns_tot.Size = new System.Drawing.Size(100, 20);
-            this.dns_tot.Text = "0";
+            this.n_requests.Location = new System.Drawing.Point(207, 13);
+            this.n_requests.Name = "n_requests";
+            this.n_requests.Size = new System.Drawing.Size(100, 20);
+            this.n_requests.Text = "0";
             // 
-            // ip_tot
+            // n_failures
             // 
-            this.ip_tot.Location = new System.Drawing.Point(207, 56);
-            this.ip_tot.Name = "ip_tot";
-            this.ip_tot.Size = new System.Drawing.Size(100, 20);
-            this.ip_tot.Text = "0";
-            // 
-            // dns_fail
-            // 
-            this.dns_fail.Location = new System.Drawing.Point(207, 104);
-            this.dns_fail.Name = "dns_fail";
-            this.dns_fail.Size = new System.Drawing.Size(100, 20);
-            this.dns_fail.Text = "0";
-            // 
-            // ip_fail
-            // 
-            this.ip_fail.Location = new System.Drawing.Point(207, 148);
-            this.ip_fail.Name = "ip_fail";
-            this.ip_fail.Size = new System.Drawing.Size(100, 20);
-            this.ip_fail.Text = "0";
+            this.n_failures.Location = new System.Drawing.Point(207, 104);
+            this.n_failures.Name = "n_failures";
+            this.n_failures.Size = new System.Drawing.Size(100, 20);
+            this.n_failures.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(320, 240);
+            this.ClientSize = new System.Drawing.Size(640, 480);
             this.ControlBox = false;
-            this.Controls.Add(this.ip_fail);
-            this.Controls.Add(this.dns_fail);
-            this.Controls.Add(this.ip_tot);
-            this.Controls.Add(this.dns_tot);
+            this.Controls.Add(this.n_failures);
+            this.Controls.Add(this.n_requests);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.dns_failures);
-            this.Controls.Add(this.nodns_total);
             this.Controls.Add(this.dns_total);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
@@ -137,14 +100,10 @@
         #endregion
 
         private System.Windows.Forms.Label dns_total;
-        private System.Windows.Forms.Label nodns_total;
         private System.Windows.Forms.Label dns_failures;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label dns_tot;
-        private System.Windows.Forms.Label ip_tot;
-        private System.Windows.Forms.Label dns_fail;
-        private System.Windows.Forms.Label ip_fail;
+        private System.Windows.Forms.Label n_requests;
+        private System.Windows.Forms.Label n_failures;
     }
 }
 
--- ModemReliabilityTest/Form1.cs
+++ ModemReliabilityTest/Form1.cs
@@ -18,10 +18,14 @@ namespace ModemReliabilityTest
         private static readonly ILog log = LogManager.GetLogger(typeof(Form1));
         System.Windows.Forms.Timer timer;
         HttpWebRequest r;
+        HttpWebResponse m_resp;
+        int DataBlockSize = 10;
+        byte[] dataBuffer;
+        FileStream m_fs;
         static string url1 = "http://drillersdashboard.com/images/productivity-circle.png";
-        static string url2 = "http://174.143.211.239/images/productivity-circle.png";
+        //static string url2 = "http://174.143.211.239/images/productivity-circle.png";
 
-        string[] urls = { url1, url2 };
+        //string[] urls = { url1, url2 };
 
         public Form1()
         {
@@ -37,51 +41,117 @@ namespace ModemReliabilityTest
             timer.Enabled = true;
         }
 
+        void oldDownload(string u)
+        {
+
+            r = (HttpWebRequest)WebRequest.Create(u);
+            using (var resp = r.GetResponse())
+            using (var s = resp.GetResponseStream())
+            using (var sr = new StreamReader(s))
+            {
+                sr.ReadToEnd();
+            }
+        }
+        
+        void doDownload(string u)
+        {
+            try
+            {
+                r = (HttpWebRequest)HttpWebRequest.Create(u);
+                r.BeginGetResponse(new AsyncCallback(ResponseReceived), null);
+            }
+            catch (Exception ex)
+            {
+                log.Error("Failed in doDownload", ex);
+                this.Invoke(new EventHandler(this.Failure));
+            }
+        }
+
+        void ResponseReceived(IAsyncResult res)
+        {
+            try
+            {
+                try
+                {
+                    m_resp = (HttpWebResponse)r.EndGetResponse(res);
+                }
+                catch (WebException ex)
+                {
+                    log.Error("error in response received", ex);
+                    this.Invoke(new EventHandler(this.Failure));
+                    return;
+                }
+                dataBuffer = new byte[DataBlockSize];
+                m_fs = new FileStream(Directory.GetCurrentDirectory() + @"\download.png",
+                                        FileMode.Create);
+                // Request the first chunk
+                m_resp.GetResponseStream().BeginRead(dataBuffer, 0, DataBlockSize,
+                  new AsyncCallback(OnDataRead), this);
+            }
+            catch (Exception ex)
+            {
+                log.Error("Failure in ResponseReceived", ex);
+                this.Invoke(new EventHandler(this.Failure));
+            }
+        }
+
+        void OnDataRead(IAsyncResult res)
+        {
+            try
+            {
+                // How many bytes did we get this time
+                int nBytes = m_resp.GetResponseStream().EndRead(res);
+                // Write buffer
+                m_fs.Write(dataBuffer, 0, nBytes);
+                // Are we done yet?
+                if (nBytes > 0)
+                {
+                    // No, keep reading
+                    m_resp.GetResponseStream().BeginRead(dataBuffer, 0,
+                      DataBlockSize, new AsyncCallback(OnDataRead), this);
+                }
+                else
+                {
+                    m_fs.Close();
+                    m_fs = null;
+                    this.Invoke(new EventHandler(this.AllDone));
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error("Failure in OnDataRead", ex);
+                this.Invoke(new EventHandler(this.Failure));
+            }
+        }
+
+        void AllDone(object sender, EventArgs e)
+        {
+            log.Info("Download complete");
+            timer.Enabled = true;
+        }
+
+        void Failure(object sender, EventArgs e)
+        {
+            log.Info("fail");
+            this.n_failures.Text = ((int)(int.Parse(this.n_failures.Text) + 1)).ToString();
+        }
+
         void timer_Tick(object sender, EventArgs e)
         {
             timer.Enabled = false;
-            foreach (string u in urls)
-            {
-                if (u == url1)
-                {
-                    this.dns_tot.Text = ((int)(int.Parse(this.dns_tot.Text) + 1)).ToString();
-                }
-                else 
-                {
-                    this.ip_tot.Text = ((int)(int.Parse(this.ip_tot.Text) + 1)).ToString();
-                }
-                try
-                {
-                    r = (HttpWebRequest)WebRequest.Create(u);
-                    using (var resp = r.GetResponse())
-                    using (var s = resp.GetResponseStream())
-                    using (var sr = new StreamReader(s))
-                    {
-                        sr.ReadToEnd();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    if (u == url1)
-                    {
-                        this.dns_fail.Text = ((int)(int.Parse(this.dns_fail.Text) + 1)).ToString();
-                    }
-                    else
-                    {
-                        this.ip_fail.Text = ((int)(int.Parse(this.ip_fail.Text) + 1)).ToString();
-                    }
-
-                    log.Error(ex);
-                }
-            }
-            log.Info("Download complete");
-            timer.Enabled = true;
+            this.n_requests.Text = ((int)(int.Parse(this.n_requests.Text) + 1)).ToString();
+            doDownload(url1);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             timer.Enabled = false;
             Application.Exit();
+        }
+
+        private void dns_total_ParentChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
